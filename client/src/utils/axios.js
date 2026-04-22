c3482cf65ef6d1aa19778200a7afc16d4a666f1d
ss-saksham <sinsaksham106@gmail.com>
2026-04-22 14:13:31 +0530

    backend-frontend-link

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: "https://eventora-server-azure.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
